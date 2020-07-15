ff61ecd8b1d22b639fb9fd10875180c89b0c0e97
Derek Chan <singularity.panda@gmail.com>
2020-07-16 03:15:49 +1000

    Added tons of functionality and included new menu for ease of use. Still work in progress!

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# api-v3-client-python
+# Developer Derek. M. D. Chan
+# Github https://github.com/NET-OF-BEING
+# BTC Markets Developer API
+
+
+        print('''
+   ______        _________    ________          _____ ______       ________      ________      ___  __        _______       _________    ________      
+  |\   __  \    |\___   ___\ |\   ____\        |\   _ \  _   \    |\   __  \    |\   __  \    |\  \|\  \     |\  ___ \     |\___   ___\ |\   ____\     
+  \ \  \|\ /_   \|___ \  \_| \ \  \___|        \ \  \\\__\ \  \   \ \  \|\  \   \ \  \|\  \   \ \  \/  /|_   \ \   __/|    \|___ \  \_| \ \  \___|_    
+   \ \   __  \       \ \  \   \ \  \            \ \  \\|__| \  \   \ \   __  \   \ \   _  _\   \ \   ___  \   \ \  \_|/__       \ \  \   \ \_____  \   
+    \ \  \|\  \       \ \  \   \ \  \____        \ \  \    \ \  \   \ \  \ \  \   \ \  \\  \|   \ \  \\ \  \   \ \  \_|\ \       \ \  \   \|____|\  \  
+     \ \_______\       \ \__\   \ \_______\       \ \__\    \ \__\   \ \__\ \__\   \ \__\\ _\    \ \__\\ \__\   \ \_______\       \ \__\    ____\_\  \ 
+
+
+
+This Python program is still in the works but most of the functionality works.
+
+The official API of [https://www.btcmarkets.net/](https://www.btcmarkets.net/)
+
+# API v3
+We have recently launched a new version of trading API that includes several improvements and new features.  
+--------------------------------------------------------------------------------------------------------------
+
+We encourage using our latest API for any new trading projects.  
+
+https://api.btcmarkets.net/doc/v3#section/Introduction
+
+--------------------------------------------------------------------------------------------------------------
+
+
+Please feel free to DONATE!  
+------------------------------------------------------ 
+Bitcoin : 16K2LKdXXt4GXKyZYvu8Q9qrWYQGaEtyNa
+LiteCoin : ML8HdH13wEwRcBMh7VHgLb8LNMYBsBdn8T
+DRGN : 0x7b4be22a721763fc6d64ccb431aa588195162544
+XRP : 632217416
+ETH : 0x7b4be22a721763fc6d64ccb431aa588195162544
+LINK : 0x7b4be22a721763fc6d64ccb431aa588195162544
+-----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
 import base64
+import yaml
+import pprint
 import hashlib
 import hmac
 import time
 import json
+import config
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError, URLError
 
@@ -72,30 +76,204 @@ class BTCMarkets:
         self.privateKey = base64.b64decode(privateKey)
 
     def get_orders(self):
-
+        print("ORDERS")
+        print('----------------------------------------------')
         return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/orders', 'status=all')
+        print('----------------------------------------------\n\n')
 
+    def open_all_orders(self):
+        print("OPEN ALL ORDERS")
+        print('----------------------------------------------\n\n')
+        instrument = input("Enter instrument: ")
+        currency  = input("Enter currency: ")
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v2/order/open[/{instrument}/{currency}', 'status=all')
+    
+    def get_order_book(self):
+        marketId  = input("Enter Market ID: ")
+        print("MARKET ID")
+        print('----------------------------------------------')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/markets/{marketId}/orderbook', marketId,'status=none')
+        print('----------------------------------------------\n\n')
+    
+    def get_order_history(self):
+        print("ORDER HISTORY")
+        print('----------------------------------------------')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/orders', 'status=none')
+        print('----------------------------------------------\n\n')
+    
+    
+    def get_order_details(self):
+        print("ORDER DETAILS")
+        print('----------------------------------------------')
+        return makeHttpCall('POST', self.apiKey, self.privateKey, '/order/detail', 'status=all')
+        print('----------------------------------------------\n\n')
+    
+    def get_trade_history(self):
+        print("TRADE HISTORY")
+        print('----------------------------------------------')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/trade','status=all')
+        print('----------------------------------------------\n\n')
+        
+    #def get_trade_history(self):
+        #instrument = input("Enter instrument: ")
+        #currency  = input("Enter currency: ")
+        #limit = input("Enter limit: ")
+        #since  = input("Enter since: ")
+        #print("TRADE HISTORY")
+        #print('----------------------------------------------')
+        #return makeHttpCall('GET', self.apiKey, self.privateKey, '/v2/order/trade/history/{instrument}/{currency}/limit={limit}&since={since}','status=all')
+        #print('----------------------------------------------\n\n')
+    
+    def account_balance(self):
+        print("ACCOUNT BALANCE")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/accounts/me/balances', 'status=all')
+
+    def get_open_orders(self):
+        print("OPEN ORDERS")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('POST', self.apiKey, self.privateKey, '/v2/order/open', 'status=all')
+    
+    def get_deposits(self):
+        print("LIST DEPOSITS")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/deposits', 'status=all')
+    
+    def deposits_withdrawals(self):
+        before = input("Before: ")
+        after = input("After: ")
+        limit = input("Limit: ")
+        print("DEPOSITS / WITHDRAWALS")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/transfers/', '?before=before&after=after&limit=limit', 'status=all')
+    
+    def get_deposit_address(self):
+        assetName = input("Enter asset: ")
+        print("COIN ADDRESS")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/addresses',assetName, 'status=all')
+
+    def withdraw(self):
+        print("ACCOUNT BALANCE")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/accounts/me/balances', 'status=all')
+    
     def place_new_order(self):
+        marketID = input("Enter Market ID: ")
+        price= input("Enter Price: ")
+        amount = input("Enter Amount: ")
+        orderType= input("Enter Order Type: ")
+        orderSide= input("Enter Order Side: ")
 
         model = {
-            'marketId': 'BTC-AUD',
-            'price': '100.12',
-            'amount': '1.034',
-            'type': 'Limit',
-            'side': 'Bid'
+            'marketID': marketID,
+            'price': price,
+            'amount': amount,
+            'type': orderType,
+            'side': orderSide
         }
-
         return makeHttpCall('POST', self.apiKey, self.privateKey, '/v3/orders', None, model)
 
-    def cancel_order(self):
+    def replace_order(self):
+        orderID = input("Enter Order ID: ")
+        price= input("Enter Price: ")
+        amount = input("Enter Amount: ")
+        clientOrderId = input("Enter Amount: ")
+        model = {
+            'price': price,
+            'amount': amount,
+            'clientOrderId': clientOrderId,
+        }
+        return makeHttpCall('PUT', self.apiKey, self.privateKey, '/v3/orders/',id,model)
 
-        return makeHttpCall('DELETE', self.apiKey, self.privateKey, '/v3/orders', 'id=1224905')
+    def cancel_order_id(self):
+        orderID = input("Enter OrderID: ")
+        print("CANCEL ORDER BY ID")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('DELETE', self.apiKey, self.privateKey, '/v3/orders/',orderID)
+    
+    def server_time(self):
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/time','status=none')
+    
+    def get_asset(self):
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/assets','status=none')
+    
+    def cancel_order_market(self):
+        marketID = input("MarketID: ")
+        print("CANCEL ORDER BY MARKET")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('DELETE', self.apiKey, self.privateKey, '/v3/orders',marketID)
+    
+    def withdrawal_fees(self):
+        print("WITHDRAWAL FEES")
+        print('----------------------------------------------\n\n')
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/withdrawal-fees', 'status=none')
+    
+    def my_transactions(self):
+        coin = input("Enter Coin: ")
+        return makeHttpCall('GET', self.apiKey, self.privateKey, '/v3/accounts/me/transactions/',coin)
 
-api_key = 'add api key here'
-private_key = 'add private key here'
 
-client = BTCMarkets(api_key, private_key)
 
-# print(client.get_orders())
-# print(client.place_new_order())
-# print(client.cancel_order())
+if __name__ == "__main__":
+
+    api_key = config.api_key
+    print(api_key)
+    private_key = config.private_key
+    client = BTCMarkets(api_key, private_key)
+    menuchoices = {'1':client.account_balance, '2':client.get_order_details,'3':client.get_order_book,'4':client.get_order_history,'5':client.get_trade_history,'6':client.open_all_orders,'7':client.get_open_orders,'8':client.get_orders,'9':client.get_deposit_address,'t':client.my_transactions,'a':client.server_time,'w':client.withdrawal_fees,'c':client.get_asset,'x':client.cancel_order_market,'l':client.cancel_order_id,'v':client.get_deposits,'g':client.place_new_order,'r':client.replace_order,'y':client.deposits_withdrawals}
+
+    def menu(self):
+        print('''
+   ______        _________    ________          _____ ______       ________      ________      ___  __        _______       _________    ________      
+  |\   __  \    |\___   ___\ |\   ____\        |\   _ \  _   \    |\   __  \    |\   __  \    |\  \|\  \     |\  ___ \     |\___   ___\ |\   ____\     
+  \ \  \|\ /_   \|___ \  \_| \ \  \___|        \ \  \\\__\ \  \   \ \  \|\  \   \ \  \|\  \   \ \  \/  /|_   \ \   __/|    \|___ \  \_| \ \  \___|_    
+   \ \   __  \       \ \  \   \ \  \            \ \  \\|__| \  \   \ \   __  \   \ \   _  _\   \ \   ___  \   \ \  \_|/__       \ \  \   \ \_____  \   
+    \ \  \|\  \       \ \  \   \ \  \____        \ \  \    \ \  \   \ \  \ \  \   \ \  \\  \|   \ \  \\ \  \   \ \  \_|\ \       \ \  \   \|____|\  \  
+     \ \_______\       \ \__\   \ \_______\       \ \__\    \ \__\   \ \__\ \__\   \ \__\\ _\    \ \__\\ \__\   \ \_______\       \ \__\    ____\_\  \ 
+      \|_______|        \|__|    \|_______|        \|__|     \|__|    \|__|\|__|    \|__|\|__|    \|__| \|__|    \|_______|        \|__|   |\_________\
+                                                                                                                                           \|_________|
+        ''')
+        print("---------------------------------")
+        print("Select an option:")
+        print("---------------------------------\n")
+        print("1. Account Balance ")
+        print("2. Order Details")
+        print("3. Get Order Book")
+        print("4. Order History")
+        print("5. Trade History")
+        print("6. Open All Orders")
+        print("7. Open Orders")
+        print("8. Orders")
+        print("9. Deposit Address")
+        print("v, List Deposits")
+        print("t. My Transactions")
+        print("a. Server Time")
+        print("x. Cancel Order by Market ID")
+        print("l. Cancel Order by Order ID")
+        print("0. Sell quote")
+        print("o. My Orders")
+        print("h. Orders History")
+        print("w. Withdrawal Fees")
+        print("y. List Deposits/Withdrawals")
+        print("c. Assets")
+        print("r. Replace an Order")
+        print("g. Place Buy Order")
+        print("p. Place Sell Order")
+        print("q. Quit\n\n")
+        print("----------------------------------\n")
+    
+        ret = menuchoices[input("Option: ")]()
+        pprint.pprint(ret)
+        #if ret is None:
+        #    print("Please enter a valid menu choice!")
+        #menuchoices['q']()
+
+menu(client)
+#print(order)
+#print(yaml.dump(order, default_flow_style="True", default_style=""))
+#pprint.pprint(client.account_balance())
+#print(yaml.dump(client.place_new_order()))
+#print(yaml.dump(client.cancel_order()))
+#pprint.pprint(yaml.dump(client.get_trade_history()))
+#pprint.pprint(yaml.dump(client.get_order_book()))
